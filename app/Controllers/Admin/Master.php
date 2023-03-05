d961ee8bc8813b319505cf83abf5519d757eb222
GungDan21 <dannyfontana8@gmail.com>
2023-03-05 16:44:56 +0700

    Bootstrap

---
+++ app/Controllers/Admin/Master.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers\Admin;
+use App\Controllers\BaseController;
+
+class Master extends BaseController
+{
+    public function index()
+    {
+        echo "Hi, ini adalah halaman Master";
+    }
+
+    
+}
